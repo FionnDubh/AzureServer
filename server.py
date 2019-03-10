8f7479858b0000ff4b550c0150b4c4500090ef0b
FionnDubh <lillerover@gmail.com>
2019-03-10 00:00:10 +0000

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+import multiprocessing
+import socket
+import os
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/status")
+def returnProperties():
+        return """{
+            "hostname": \"""" + str(socket.gethostname()) + """,\"
+                "ip_address": \"""" + str(socket.gethostbyname(socket.gethostname())) + """,\"
+                    "cpus": \"""" + str(multiprocessing.cpu_count()) + """,\"
+                        "memory": \"""" + str(os.sysconf('SC_PAGE_SIZE')* os.sysconf('SC_PHYS_PAGES')) + """\"
+                            }"""
