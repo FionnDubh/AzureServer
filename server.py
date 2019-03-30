8430fd3373c90d1f48df8a1810f1bb0c54b50d39
Ubuntu <lille@DariaVM1.gddnmzs1orheffbwtrhjsjkbch.fx.internal.cloudapp.net>
2019-03-30 16:02:15 +0000

    fixed

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route("/status")
 def returnProperties():
         return """{
-            "hostname": \"""" + str(socket.gethostname()) + """,\"
-                "ip_address": \"""" + str(socket.gethostbyname(socket.gethostname())) + """,\"
-                    "cpus": \"""" + str(multiprocessing.cpu_count()) + """,\"
+            "hostname": \"""" + str(socket.gethostname()) + """\",
+                "ip_address": \"""" + str(socket.gethostbyname(socket.gethostname())) + """\",
+                    "cpus": \"""" + str(multiprocessing.cpu_count()) + """\",
                         "memory": \"""" + str(os.sysconf('SC_PAGE_SIZE')* os.sysconf('SC_PHYS_PAGES')) + """\"
                             }"""
